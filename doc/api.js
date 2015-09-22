0f60c24a6cb0ebdde761f45a67b2307cae296108
shinout <shinout310@gmail.com>
2015-09-23 07:35:36 +0900

    update doc

---
+++ doc/api.js
@@ -0,0 +1,21 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Interval",
+        "IntervalTree",
+        "Node",
+        "Point",
+        "SortedList"
+    ],
+    "modules": [
+        "interval-tree2"
+    ],
+    "allModules": [
+        {
+            "displayName": "interval-tree2",
+            "name": "interval-tree2",
+            "description": "interval tree"
+        }
+    ]
+} };
+});
